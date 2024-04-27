2daadf44ba98743d37ff3a53a4c93231c7aa48b7
Emith Dinsara <88488807+EmithD@users.noreply.github.com>
2024-04-27 20:04:25 +0530

    Update page.tsx

--- app/join-us/page.tsx
+++ app/join-us/page.tsx
@@ -1,9 +1,33 @@
 import { title } from "@/components/primitives";
+import { siteConfig } from "@/config/site";
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import Link from "next/link";
+
 
 export default function JoinPage() {
 	return (
 		<div>
+			
 			<h1 className={title()}>Join Us</h1>
+			<div className="">
+				<div className="pb-4 pt-4">
+					<h1 className={title({ color: "cyan", size: "sm", fullWidth: true })}>All we need is your email</h1>
+				</div>
+				<form action="" className="flex flex-row gap-10">
+					<Input
+					isRequired
+					type="email"
+					label="Email"
+					defaultValue="email@email.com"
+					className="max-w-full"
+					/>
+					<div className="pt-2">
+						<Button type="submit">Join</Button>
+					</div>
+				</form>
+			</div>
+
 		</div>
 	);
 }
